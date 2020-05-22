7fcbf5a26825ea71a3edb6ffdf69e4281be94b21
McBryan <solomonmcbryan@gmail.com>
2020-05-22 01:15:32 +0000

    Done with Get started page

--- App.js
+++ App.js
@@ -3,25 +3,36 @@ import { StyleSheet, Text, View } from "react-native";
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import GetStarted from "./screens/GetStarted";
+import HomePage from "./screens/HomePage";
+import Signin from "./screens/Signin";
+import Register from "./screens/Register";
 
 const Stack = createStackNavigator();
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Notifications" component={Notifications} />
-        <Stack.Screen name="Profile" component={Profile} />
-        <Stack.Screen name="Settings" component={Settings} />
-      </Stack.Navigator>
-    </NavigationContainer>
+      <NavigationContainer style={styles.container}>
+        <Stack.Navigator initialRouteName={GetStarted}>
+          <Stack.Screen
+            options={{
+              headerTitle: null,
+              headerStyle: { height: 0 },
+            }}
+            name="Get Started"
+            component={GetStarted}
+          />
+          <Stack.Screen name="Home Page" component={HomePage} />
+          <Stack.Screen name="Sign in" component={Signin} />
+          <Stack.Screen name="Register" component={Register} />
+        </Stack.Navigator>
+      </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#000",
     alignItems: "center",
     justifyContent: "center",
   },
--- screens/GetStarted.js
+++ screens/GetStarted.js
@@ -1,14 +1,58 @@
-import React, { Component } from 'react'
-import { Text, StyleSheet, View } from 'react-native'
+import React, { Component } from "react";
+import { Text, StyleSheet, View, Image } from "react-native";
+import logo from "../assets/images/logo.png";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 export default class GetStarted extends Component {
-    render() {
-        return (
-            <View>
-                <Text> textInComponent </Text>
-            </View>
-        )
-    }
+  render() {
+    const {navigation} = this.props
+    return (
+      <View style={styles.container}>
+        <View>
+          <Image source={logo} style={styles.image} />
+        </View>
+        <View style={styles.contact__text__container}>
+          <Text style={styles.contact__text}>Contact Sharer</Text>
+        </View>
+
+        <View style={styles.getstarted__text__container}>
+          <TouchableOpacity onPress={()=>{navigation.navigate('Home Page')}}>
+            <Text style={styles.getstarted__text}>Get Started</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#000",
+    alignItems: "center",
+    flexDirection: "column",
+    flex: 1,
+  },
+  image: {
+    height: 45,
+    width: 240,
+    marginVertical: 150,
+  },
+  contact__text__container: {
+    alignItems: "center",
+    marginBottom: 150,
+  },
+  contact__text: {
+    color: "whitesmoke",
+    fontSize: 30,
+    fontWeight: "bold",
+    letterSpacing: 4,
+  },
+  getstarted__text:{
+    color: "whitesmoke",
+    fontSize: 20,
+    fontWeight: "bold",
+    letterSpacing: 4,
+    borderBottomWidth: 10,
+    borderBottomColor: 'red'
+  }
+});
